ab4fd30ab435999a35c9e3437a64d6997b8d3bf9
terad <terada.aki@auf.edu.ph>
2023-09-15 23:32:50 +0800

    Add project files.

---
+++ Mod02Act02/Mod02Act02/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod02Act02.Model
+{
+    internal class Student
+    {
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+        public int Unit { get; set; }
+    }
+}
